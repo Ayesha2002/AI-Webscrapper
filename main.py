1b2ae9a3a008363ad08ec4bf4f9e46ec3078ffcb
Ayesha Khan <ayesha200210@gmail.com>
2025-02-05 15:43:36 +0530

    commit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-#UI for strramlit and for website to enter the details as per the user.
+#UI for streamlit and for website to enter the details as per the user.
 
 import streamlit as st
-
+from scrape import scrape_website
 
 st.title(" AI Web Scraper")
 url = st.text_input("Enter a website URL:")
 
 if st.button("Scrape Site"):
     st.write("Scraping the Website")
+    result = scrape_website(url)
+    print(result)
--- scrape.py
+++ scrape.py
@@ -1,2 +1,29 @@
 #Web scraping via Selenium
-#
+#Creation of function or module for taking URL and scrape the content
+# Selenium controls our web browser like interaction with website
+
+
+import selenium.webdriver as webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+def scrape_website(website):
+    print("Launching chrome browser...")
+
+    chrome_driver_path ="./chromedriver.exe"
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service= Service(chrome_driver_path), options=options)
+
+
+    try:
+        driver.get(website)
+        print("Page loaded...")
+        html = driver.page_source
+        time.sleep(10)
+
+        return html
+    finally:
+        driver.quit()
+
+
+
